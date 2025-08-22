2057b27882e6b7719ecda4f1545aaf48c4ded014
ThangVS <thangvuong2512@gmail.com>
2025-08-22 22:30:13 +0700

    menucsrean(note cho huy là mo menuscrean nha khong phai menu )

---
+++ Assets/Scripts/Menu/Launcher.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using System.Collections.Generic;
+using Photon.Realtime;
+using System.Linq;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+    [SerializeField] InputField roomNameInputField;
+    [SerializeField] Text roomNameText;
+    [SerializeField] Text errorText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject playerListItemPrefab;
+    public GameObject startbutton;
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        Debug.Log("Connecting to Master ...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.instance.OpenMenu("TitleMenu");
+        Debug.Log("Joined Lobby");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.instance.OpenMenu("RoomMenu");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach (Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        startbutton.SetActive(PhotonNetwork.IsMasterClient);
+
+    }
+
+    // 0 references
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startbutton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Generation Unsuccesfull" + message;
+        MenuManager.instance.OpenMenu("ErrorMenu");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.instance.OpenMenu("TitleMenu");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList) continue;
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
+    }
+}
---
+++ Assets/Scripts/Menu/RoomListItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun.Demo.PunBasics;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField] Text roomNameText;
+    public RoomInfo info;
+
+    public void SetUp(RoomInfo _info)
+    {
+        info = _info;
+        roomNameText.text = _info.Name;
+    }
+
+    public void onClick()
+    {
+        Launcher.instance.JoinRoom(info);
+    }
+}
